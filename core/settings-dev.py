072a935a2f2a1d28ffef9009e18f30fae6bf3944
Ali <aliahadmd1@outlook.com>
2024-12-27 13:10:49 +0600

    feat: update static file settings and enhance home template with Bootstrap and a form

--- core/settings-dev.py
+++ core/settings-dev.py
@@ -89,8 +89,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = str(BASE_DIR / 'static')
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    str(BASE_DIR / 'static'),
+]
 
 # Media files
 MEDIA_URL = '/media/'
